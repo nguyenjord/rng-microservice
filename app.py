44223aa5bc654ae13f5f42765a3ebbbde035cb9d
nguyenjord <nguyenjordan1023@gmail.com>
2025-11-30 01:09:48 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import zmq, json, random
+
+
+def gen_rand(min, max):
+    return random.randint(min, max)
+
+def gen_rand_list(min, max, count):
+    return [random.randint(min, max) for _ in range(count)]
+
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5557")
+    print("Random number generator microservice running on port 5557")
+
+    while True:
+        message = socket.recv_string()
+        request = json.loads(message)
+
+        method = request.get("method")
+        params = request.get("params",{})
+
+        if method == "rand":
+            response = gen_rand(params["min"],params["max"])
+
+        elif method == "rand_list":
+            response = gen_rand_list(params["min"], params["max"], params["count"])
+        else:
+            response = {"error": "Unknown value"}
+
+        socket.send_string(json.dumps(response))
+
+if __name__ == "__main__":
+    main()
